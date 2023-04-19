636958c361f8b3bbb1deac6c8a60be3d2cef55b2
AndyNtiamoah <126166717+AndyNtiamoah@users.noreply.github.com>
2023-04-19 11:37:26 +0200

    Add files via upload

--- capp/__init__.py
+++ capp/__init__.py
@@ -7,10 +7,10 @@ from flask_login import LoginManager
 
 application = Flask(__name__)
 
-application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 
-# application.config['SECRET_KEY'] = '23b3436c2bd9f7ee888955a2611f160471f78097432f6d3d'
-DBVAR = 'postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SECRET_KEY'] = '23b3436c2bd9f7ee888955a2611f160471f78097432f6d3d'
+DBVAR = 'postgresql://postgres:123Japanognorge@awseb-e-ak2cfptjvy-stack-awsebrdsdatabase-kjgmzyyuqnqj.cclddedhaznl.eu-north-1.rds.amazonaws.com:5432/ebdb'
 # databases for users and transport 
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
